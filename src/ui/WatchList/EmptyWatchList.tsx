96cae46ccfec225f123d7686ad26c7d0398c4f05
alexcherman <sashacherman@gmail.com>
2020-06-10 07:17:12 +0200

    Chage empty watchlist text

---
+++ src/ui/WatchList/EmptyWatchList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import starIcon from '../../assets/icons/star.svg';
+
+export const EmptyWatchList = () => (
+  <EmptyView>
+    <img src={starIcon} alt="star"/>
+    <EmptyViewTitle>You have not added any assets to your watchlist</EmptyViewTitle>
+    <EmptyViewText>Use the Asset Leaderboard to build your watchlist</EmptyViewText>
+  </EmptyView>
+);
+
+const EmptyView = styled.div`
+  padding: 170px 16px;
+  background: ${({ theme }) => theme.colors.backgroundPrimary};
+  border-radius: 2px;
+  text-align: center;
+`;
+
+const EmptyViewTitle = styled.h2`
+  margin: 8px 0;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 29px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.colorPrimary};
+`;
+
+const EmptyViewText = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.colorSecondary};
+`;
--- src/ui/WatchList/WatchList.tsx
+++ src/ui/WatchList/WatchList.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Container } from '../common/Container';
 import { ButtonsRow } from '../common/Button/ButtonsRow';
 import { Tabs, TabsProps } from '../common/Tabs';
-import starIcon from '../../assets/icons/star.svg';
+import { EmptyWatchList } from './EmptyWatchList';
 
 export const WatchList = (props: TabsProps) => (
   <>
@@ -11,35 +10,7 @@ export const WatchList = (props: TabsProps) => (
       <ButtonsRow>
         <Tabs {...props}/>
       </ButtonsRow>
-      <EmptyView>
-        <img src={starIcon} alt="star"/>
-        <EmptyViewTitle>You don’t have any assets added to the watchlist</EmptyViewTitle>
-        <EmptyViewText>Use asset list to create watchlist</EmptyViewText>
-      </EmptyView>
+      <EmptyWatchList/>
     </Container>
   </>
 );
-
-const EmptyView = styled.div`
-  padding: 170px 16px;
-  background: ${({ theme }) => theme.colors.backgroundPrimary};
-  border-radius: 2px;
-  text-align: center;
-`;
-
-const EmptyViewTitle = styled.h2`
-  margin: 8px 0;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 29px;
-  text-align: center;
-  color: ${({ theme }) => theme.colors.colorPrimary};
-`;
-
-const EmptyViewText = styled.p`
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 19px;
-  text-align: center;
-  color: ${({ theme }) => theme.colors.colorSecondary};
-`;
