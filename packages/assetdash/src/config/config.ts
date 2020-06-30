8ed42ca84f7bb8cc814040aa44e7cd875ed2af89
Vlad Starostenko <vlad.starostenko.0501@gmail.com>
2020-06-30 17:27:21 +0200

    Export headers to corsMiddleware

--- packages/assetdash/src/config/config.ts
+++ packages/assetdash/src/config/config.ts
@@ -1,7 +1,6 @@
 import {getProdConfig} from './production';
 import {getDevConfig} from './development';
 import {getTestConfig} from './test';
-import {Request, Response, NextFunction} from 'express';
 
 let configObject;
 
@@ -20,11 +19,5 @@ switch (process.env.NODE_ENV) {
 }
 
 export const config = Object.freeze(configObject);
-
-export const setHeaders = (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', config.accessControlAllowOrigin);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-};
 
 export type Config = typeof config;
