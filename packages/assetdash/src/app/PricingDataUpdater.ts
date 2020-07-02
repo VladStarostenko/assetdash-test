dde5bf3b97fb7a2269aa6120880c0fd1a86052aa
VladStarostenko <38181661+VladStarostenko@users.noreply.github.com>
2020-07-02 16:05:24 +0200

    Add PricingDataUpdater to loop (#22)

--- packages/assetdash/src/app/PricingDataUpdater.ts
+++ packages/assetdash/src/app/PricingDataUpdater.ts
@@ -3,19 +3,39 @@ import {AssetRepository} from '../integration/db/repositories/AssetRepository';
 import {convertAssetDataToAssetPricingData} from '../core/utils';
 
 export class PricingDataUpdater {
+  get running(): boolean {
+    return this._running;
+  }
+
+  set running(value: boolean) {
+    this._running = value;
+  }
+
   private coinmarketCapService: CoinmarketCapService;
   private assetRepository: AssetRepository;
+  private _running: boolean;
 
   constructor(coinmarketCapService: CoinmarketCapService, assetRepository: AssetRepository) {
     this.coinmarketCapService = coinmarketCapService;
     this.assetRepository = assetRepository;
+    this._running = true;
+  }
+
+  start = () => {
+    this._running = true;
+  }
+
+  stop = () => {
+    this._running = false;
   }
 
   updateAssetPrices = async (tickers: string[]) => {
-    const assets = (await this.coinmarketCapService.getAssetsData(tickers))['data'];
-    for (const asset in assets) {
-      const pricingData = await convertAssetDataToAssetPricingData(assets[asset]);
-      await this.assetRepository.updatePrice(pricingData);
+    while (this._running) {
+      const assets = (await this.coinmarketCapService.getAssetsData(tickers))['data'];
+      for (const asset in assets) {
+        const pricingData = await convertAssetDataToAssetPricingData(assets[asset]);
+        await this.assetRepository.updatePrice(pricingData);
+      }
     }
   }
 }
