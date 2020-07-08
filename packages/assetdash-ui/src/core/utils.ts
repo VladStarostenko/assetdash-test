29d82a4558363ab5cb79df6936013b50cd0df040
VladStarostenko <vlad.starostenko.0501@gmail.com>
2020-07-08 14:31:58 +0200

    Add thousands separator

--- packages/assetdash-ui/src/core/utils.ts
+++ packages/assetdash-ui/src/core/utils.ts
@@ -1,11 +1,16 @@
 export const formatMarketcap = (marketcap: number) => {
-  return marketcap.toFixed(0);
+  return addSeparaters(marketcap.toFixed(0));
 };
 
 export const formatPrice = (price: number) => {
-  return price > 1.1 ? price.toFixed(2) : price.toFixed(6);
+  const priceAsString = price > 1.1 ? price.toFixed(2) : price.toFixed(6);
+  return addSeparaters(priceAsString);
 };
 
 export const formatChange = (change: number) => {
-  return change.toFixed(2);
+  return addSeparaters(change.toFixed(2));
+};
+
+const addSeparaters = (numberAsString: string) => {
+  return numberAsString.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 };
