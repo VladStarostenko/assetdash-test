65dbae3f10f3b05b0c74808c154b7e7bc7a256dd
VladStarostenko <vlad.starostenko.0501@gmail.com>
2020-07-08 15:07:33 +0200

    Add test for addSeperators function

--- packages/assetdash-ui/src/core/utils.ts
+++ packages/assetdash-ui/src/core/utils.ts
@@ -1,16 +1,16 @@
 export const formatMarketcap = (marketcap: number) => {
-  return addSeparaters(marketcap.toFixed(0));
+  return addSeparators(marketcap.toFixed(0));
 };
 
 export const formatPrice = (price: number) => {
   const priceAsString = price > 1.1 ? price.toFixed(2) : price.toFixed(6);
-  return addSeparaters(priceAsString);
+  return addSeparators(priceAsString);
 };
 
 export const formatChange = (change: number) => {
-  return addSeparaters(change.toFixed(2));
+  return addSeparators(change.toFixed(2));
 };
 
-const addSeparaters = (numberAsString: string) => {
+export const addSeparators = (numberAsString: string) => {
   return numberAsString.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 };
--- packages/assetdash-ui/test/core/utils.test.ts
+++ packages/assetdash-ui/test/core/utils.test.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {formatMarketcap, formatPrice} from '../../src/core/utils';
+import {formatMarketcap, formatPrice, addSeparators} from '../../src/core/utils';
 
 describe('Utils', () => {
   it('format marketcap', () => {
@@ -11,5 +11,10 @@ describe('Utils', () => {
     expect(formatPrice(10.000000000000000000)).to.eq('10.00');
     expect(formatPrice(1.100000000000000000)).to.eq('1.100000');
     expect(formatPrice(0.990000000000000000)).to.eq('0.990000');
+  });
+
+  it('add thousands separator', () => {
+    expect(addSeparators('1111111.1111111')).to.eq('1,111,111.1,111,111');
+    expect(addSeparators('-1111.1111')).to.eq('-1,111.1,111');
   });
 });
