0a5983b556cf8de8a58a2500409daa8095365543
VladStarostenko <vlad.starostenko.0501@gmail.com>
2020-07-16 09:23:39 +0200

    Mark query parameters as optional

--- packages/assetdash/src/ui/routes/assets.ts
+++ packages/assetdash/src/ui/routes/assets.ts
@@ -1,7 +1,7 @@
 import {Services} from '../../core/createServices';
 import {Router as expressRouter} from 'express';
 import {asyncHandler, sanitize, responseOf} from '@restless/restless';
-import {asNumber, asObject} from '@restless/sanitizers';
+import {asNumber, asObject, asOptional} from '@restless/sanitizers';
 
 export const assets = ({assetRepository}: Services) => {
   const router = expressRouter();
@@ -16,11 +16,11 @@ export const assets = ({assetRepository}: Services) => {
   router.get('/', asyncHandler(
     sanitize({
       query: asObject({
-        currentPage: asNumber,
-        perPage: asNumber
+        currentPage: asOptional(asNumber),
+        perPage: asOptional(asNumber)
       })
     }),
-    async ({query}) => responseOf(query.perPage && query.currentPage
+    async ({query}) => responseOf(query.currentPage && query.perPage
       ? await assetRepository.findPage(query.currentPage, query.perPage)
       : await assetRepository.findAll())
   ));
