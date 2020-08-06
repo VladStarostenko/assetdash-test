8fdfa9e78c865d16c99d90bb7fd49780f9d55344
VladStarostenko <vlad.starostenko.0501@gmail.com>
2020-08-06 13:06:50 +0200

    Improve query

--- packages/assetdash/src/integration/db/seeds/04_update_sectors.ts
+++ packages/assetdash/src/integration/db/seeds/04_update_sectors.ts
@@ -4,17 +4,20 @@ export const seed = async function (knex) {
   const idsOfAssetsFromGoldToHealth =
     (await knex('assets').select('id').whereIn('ticker', ['PG', 'NVO', 'TXG']))
       .map(id => id.id);
-  await knex('assets_tags').whereIn('assetId', idsOfAssetsFromGoldToHealth).update({tagId: tagIdOf('Health')});
+  await knex('assets_tags').whereIn('assetId', idsOfAssetsFromGoldToHealth)
+    .andWhere('tagId', tagIdOf('Gold')).update({tagId: tagIdOf('Health')});
 
   const idsOfAssetsRemoveFromGold =
     (await knex('assets').select('id').whereIn('ticker', ['RSG', 'PRU', 'CG']))
       .map(id => id.id);
-  await knex('assets_tags').whereIn('assetId', idsOfAssetsRemoveFromGold).del();
+  await knex('assets_tags').whereIn('assetId', idsOfAssetsRemoveFromGold)
+    .andWhere('tagId', tagIdOf('Gold')).del();
 
   const idOfAssetRemoveFromHospitality =
     (await knex('assets').select('id').where('ticker', '=', 'MDT'))
       .map(id => id.id);
-  await knex('assets_tags').whereIn('assetId', idOfAssetRemoveFromHospitality).del();
+  await knex('assets_tags').whereIn('assetId', idOfAssetRemoveFromHospitality)
+    .andWhere('tagId', tagIdOf('Hospitality')).del();
 
   const idsOfAssetsAddToCars =
     (await knex('assets').select('id').whereIn('ticker', ['BMWYY', 'VWAGY']))
