330921c321b6f2f620e86d2cf81bd147ac910453
VladStarostenko <vlad.starostenko.0501@gmail.com>
2020-09-09 16:45:00 +0300

    Improve ad size

--- packages/assetdash-ui/src/ui/react/common/Advertisement.tsx
+++ packages/assetdash-ui/src/ui/react/common/Advertisement.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import ad from '../../assets/ad/Assetdash@2x.png';
+import adImage from '../../assets/ad/Assetdash@2x.png';
 import styled from 'styled-components';
 
 export const Advertisement = () => (
-  <div className="container">
+  <div>
     <a
       href="https://unstoppabledomains.com/r/da702815756540f"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <Add src={ad} alt="ad"/>
+      <Ad/>
     </a>
   </div>
 );
 
-const Add = styled.img`
+const Ad = styled.div`
   width: 100%;
+  height: 37px;
+  background-image: url(${adImage});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 `;
